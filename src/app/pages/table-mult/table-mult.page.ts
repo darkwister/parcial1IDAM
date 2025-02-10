950b30157a7b883dac2f78b03d54224f31693322
DarkWister <darkwistercrak17@gmail.com>
2025-02-10 16:28:47 -0400

    multiplication table module added

--- src/app/pages/table-mult/table-mult.page.ts
+++ src/app/pages/table-mult/table-mult.page.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonList, IonItem, IonText, IonButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-table-mult',
   templateUrl: './table-mult.page.html',
   styleUrls: ['./table-mult.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonList, IonItem, IonText, IonButton]
 })
-export class TableMultPage implements OnInit {
+export class TableMultPage {
+  inputNumber?: number;
+  lastNumber: number = 0;
+  tableMult: {num: number, result: number}[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  generateTable() {
+    this.tableMult = [];
+    if(this.inputNumber){
+      this.lastNumber = this.inputNumber;
+      for(let x = 1; x <= 14; x++){
+        this.tableMult.push({num: x, result: this.lastNumber * x});
+      }
+    }
   }
-
 }
