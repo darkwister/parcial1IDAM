3b963b9953165bda0c5e07d92f50233123b5548a
DarkWister <darkwistercrak17@gmail.com>
2025-02-10 17:06:07 -0400

    comparer and avg module added

--- src/app/pages/comparer-avg/comparer-avg.page.ts
+++ src/app/pages/comparer-avg/comparer-avg.page.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonText, IonButton, IonList, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-comparer-avg',
   templateUrl: './comparer-avg.page.html',
   styleUrls: ['./comparer-avg.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonText, IonButton, IonList, IonItem]
 })
-export class ComparerAvgPage implements OnInit {
-
+export class ComparerAvgPage {
+  num1?: number;
+  num2?: number;
+  num3?: number;
+  lastNum1?: number;
+  lastNum2?: number;
+  lastNum3?: number;
+  biggestNumber?:number;
+  avg?:number;
   constructor() { }
 
-  ngOnInit() {
+  compareNumbers(){
+    this.lastNum1 = this.num1
+    this.lastNum2 = this.num2
+    this.lastNum3 = this.num3
+    if(Number(this.num1) >= Number(this.lastNum2) && Number(this.num1) >= Number(this.lastNum3)){
+      this.biggestNumber = Number(this.lastNum1)
+    }
+    else if(Number(this.num2) >= Number(this.lastNum1) && Number(this.lastNum2) >= Number(this.lastNum3)){
+      this.biggestNumber = Number(this.lastNum2)
+    }
+    else if(Number(this.num3) >= Number(this.lastNum1) && Number(this.lastNum3) >= Number(this.lastNum2)){
+      this.biggestNumber = Number(this.lastNum3)
+    }
+    else{
+      this.biggestNumber = 0 
+    }
+
+    this.avg = this.promedioDeNumeros(Number(this.lastNum1), Number(this.lastNum2), Number(this.lastNum3))
+    this.num1 = undefined;
+    this.num2 = undefined;
+    this.num3 = undefined;
   }
 
+  promedioDeNumeros(num1: number, num2: number, num3: number): number{
+    return (num1 + num2 + num3)/3
+  } 
 }
